24859d2c565d9cfa659c7d63952b7d6bda915a8c
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2023-02-14 09:42:32 +0100

    Ajout du bouton ajouter notifiaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import Card from './components/Card';
-import FoldableCard from './components/FoldableCard';
-import Cards from './components/Cards';
-import cardsData from './constants/cardsData';
+import React from "react";
+import Cards from "./components/Cards";
+import cardsData from "./constants/cardsData";
+import useNotificationData from "./hooks/useNotificationData";
 
 function App() {
+  const notif = useNotificationData();
+  const taille = notif.notificationsData.length;
   return (
     <div className="app">
       <header className="app__header header">
         <h1 className="header__title">Introduction to React</h1>
       </header>
       <main className="app__main">
+        <button className="btn" onClick={() => notif.addNotification}> Add a notifiaction</button>
         <Cards className="cards" cardsData={cardsData} />
       </main>
-      <footer className="app__footer footer">footer</footer>
+      <footer className="app__footer footer"> {`${taille} notification${(taille > 0) ? 's' : ''}`} </footer>
     </div>
   );
 }
